bdbeabe51c057f7108ac404dba2b8070422036d6
Christian Blume <chr.blume@gmail.com>
2017-12-04 21:44:14 +1300

    clean up

--- src/circular_buffer.h
+++ src/circular_buffer.h
@@ -1,46 +1,36 @@
 #pragma once
 
 
-/**
- * A simple circular buffer on the stack with a size fixed at compile time.
- * ValueType must support default construction. The buffer let's you push
- * new values onto the back and pop old values off the front.
- */
+// A simple circular buffer with a size fixed at compile time.
+// ValueType must support default construction. The buffer let's you push
+// new values onto the back and pop old values off the front.
 template<typename ValueType, int Size>
 class circular_buffer {
 public:
     using value_type = ValueType;
-    /**
-     * Constructor. Size must be at least 1
-     */
-    circular_buffer() : index_{}, populated_{}, front_{}, data_{} {
-        static_assert(Size >= 1, "Size must be at least 1");
-    }
-    /**
-     * Pushes a new value onto the end of the buffer. If that exceeds the size
-     * of the buffer then the oldest value gets dropped (the one at the front).
-     */
+
+    static_assert(Size >= 1, "Size must be at least 1");
+
+    // Pushes a new value onto the end of the buffer. If that exceeds the size
+    // of the buffer then the oldest value gets dropped (the one at the front).
     void push_back(const value_type& value) {
         data_[index_] = value;
         increment();
     }
-    /**
-     * push_back() overload for r-value references
-     */
+
+    // push_back() overload for r-value references
     void push_back(value_type&& value) {
         data_[index_] = std::move(value);
         increment();
     }
-    /**
-     * Returns the value at the front of the buffer (the oldest value)
-     */
-    const value_type& front() const noexcept {
+
+    // Returns the value at the front of the buffer (the oldest value)
+    const value_type& front() const {
         return data_[front_];
     }
-    /**
-     * Removes the value at the front of the buffer (the oldest value).
-     * Returns true if the front was popped off, false otherwise
-     */
+
+    // Removes the value at the front of the buffer (the oldest value).
+    // Returns true if the front was popped off, false otherwise
     bool pop_front() {
         if (populated_ > 0) {
             data_[front_] = ValueType{};
@@ -50,35 +40,31 @@ public:
             return false;
         }
     }
-    /**
-     * Returns the size of the buffer
-     */
-    static constexpr int size() noexcept {
+
+    // Returns the size of the buffer
+    static constexpr int size() {
         return Size;
     }
-    /**
-     * Returns the number of populated values of the buffer. Its maximum value
-     * equals the size of the buffer
-     */
-    int populated() const noexcept {
+
+    // Returns the number of populated values of the buffer. Its maximum value
+    // equals the size of the buffer
+    int populated() const {
         return populated_;
     }
-    /**
-     * Returns whether the buffer is empty
-     */
-    bool empty() const noexcept {
+
+    // Returns whether the buffer is empty
+    bool empty() const {
         return populated_ == 0;
     }
-    /**
-     * Returns whether the buffer is full
-     */
-    bool full() const noexcept {
+
+    // Returns whether the buffer is full
+    bool full() const {
         return populated_ == Size;
     }
 
 private:
 
-    void increment_or_wrap(int& value) const noexcept {
+    void increment_or_wrap(int& value) const {
         if (value == Size - 1) {
             value = 0;
         } else {
@@ -86,7 +72,7 @@ private:
         }
     }
 
-    void increment() noexcept {
+    void increment() {
         increment_or_wrap(index_);
         if (populated_ == Size) {
             increment_or_wrap(front_);
@@ -95,14 +81,14 @@ private:
         }
     }
 
-    void decrement() noexcept {
+    void decrement() {
         increment_or_wrap(index_);
         increment_or_wrap(front_);
         --populated_;
     }
 
-    int index_;
-    int populated_;
-    int front_;
-    value_type data_[Size];
+    int index_{};
+    int populated_{};
+    int front_{};
+    value_type data_[Size]{};
 };
