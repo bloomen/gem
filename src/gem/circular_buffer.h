b669c3c8088d4b627b6555573e09ff12328bc07f
Christian Blume <chr.blume@gmail.com>
2018-04-24 22:24:13 +1200

    finish up the circular buffer

--- src/gem/circular_buffer.h
+++ src/gem/circular_buffer.h
@@ -1,70 +1,89 @@
 #pragma once
+#include <type_traits>
+#include <cstddef>
+
+
+namespace gem {
 
 
 // A simple circular buffer with a size fixed at compile time.
-// ValueType must support default construction. The buffer let's you push
+// ValueType must support default construction. The buffer lets you push
 // new values onto the back and pop old values off the front.
-template<typename ValueType, int Size>
-class circular_buffer {
+template<typename ValueType, std::size_t Size>
+class circular_buffer
+{
 public:
+
     using value_type = ValueType;
 
     static_assert(Size >= 1, "Size must be at least 1");
 
+    circular_buffer() = default;
+
+    // default copy/move semantics
+    circular_buffer(const circular_buffer&) = default;
+    circular_buffer& operator=(const circular_buffer&) = default;
+    circular_buffer(circular_buffer&&) = default;
+    circular_buffer& operator=(circular_buffer&&) = default;
+
     // Pushes a new value onto the end of the buffer. If that exceeds the size
     // of the buffer then the oldest value gets dropped (the one at the front).
-    void push_back(const value_type& value) {
-        data_[index_] = value;
+    template<typename T, typename = std::enable_if_t<std::is_same_v<std::decay_t<T>, value_type>>>
+    void push(T&& value)
+    {
+        data_[end_] = std::forward<T>(value);
         increment();
     }
 
-    // push_back() overload for r-value references
-    void push_back(value_type&& value) {
-        data_[index_] = std::move(value);
-        increment();
-    }
-
-    // Returns the value at the front of the buffer (the oldest value)
-    const value_type& front() const {
+    // Returns the value at the front of the buffer (the oldest value).
+    // This is undefined if the buffer is empty
+    const value_type& front() const
+    {
         return data_[front_];
     }
 
     // Removes the value at the front of the buffer (the oldest value).
     // Returns true if the front was popped off, false otherwise
-    bool pop_front() {
-        if (populated_ > 0) {
+    bool pop()
+    {
+        if (empty()) {
+            return false;
+        } else {
             data_[front_] = ValueType{};
             decrement();
             return true;
-        } else {
-            return false;
         }
     }
 
     // Returns the size of the buffer
-    static constexpr int size() {
+    static constexpr std::size_t size()
+    {
         return Size;
     }
 
     // Returns the number of populated values of the buffer. Its maximum value
     // equals the size of the buffer
-    int populated() const {
+    std::size_t populated() const
+    {
         return populated_;
     }
 
     // Returns whether the buffer is empty
-    bool empty() const {
+    bool empty() const
+    {
         return populated_ == 0;
     }
 
     // Returns whether the buffer is full
-    bool full() const {
+    bool full() const
+    {
         return populated_ == Size;
     }
 
 private:
 
-    void increment_or_wrap(int& value) const {
+    void increment_or_wrap(std::size_t& value) const
+    {
         if (value == Size - 1) {
             value = 0;
         } else {
@@ -72,23 +91,27 @@ private:
         }
     }
 
-    void increment() {
-        increment_or_wrap(index_);
-        if (populated_ == Size) {
+    void increment()
+    {
+        increment_or_wrap(end_);
+        if (full()) {
             increment_or_wrap(front_);
         } else {
             ++populated_;
         }
     }
 
-    void decrement() {
-        increment_or_wrap(index_);
+    void decrement()
+    {
         increment_or_wrap(front_);
         --populated_;
     }
 
-    int index_{};
-    int populated_{};
-    int front_{};
-    value_type data_[Size]{};
+    std::size_t end_{};
+    std::size_t front_{};
+    std::size_t populated_{};
+    value_type data_[Size];
 };
+
+
+} // gem
--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -1,13 +1,14 @@
 #include "catch.hpp"
-#include "circular_buffer.h"
+#include <gem/circular_buffer.h>
 
+using gem::circular_buffer;
 
 TEST_CASE("circular_buffer__buffer_of_size_one_no_elements") {
     constexpr int size = 1;
     circular_buffer<double, size> buffer;
     static_assert(size == buffer.size(), "");
     REQUIRE(0 == buffer.populated());
-    REQUIRE(0 == buffer.front());
+    REQUIRE(buffer.empty());
 }
 
 TEST_CASE("circular_buffer__buffer_of_size_one_with_one_added") {
@@ -15,7 +16,7 @@ TEST_CASE("circular_buffer__buffer_of_size_one_with_one_added") {
     circular_buffer<double, size> buffer;
     static_assert(size == buffer.size(), "");
     const double value = 42;
-    buffer.push_back(value);
+    buffer.push(value);
     REQUIRE(value == buffer.front());
     REQUIRE(1 == buffer.populated());
 }
@@ -26,10 +27,10 @@ TEST_CASE("circular_buffer__buffer_of_size_one_with_two_added") {
     static_assert(size == buffer.size(), "");
     const double value1 = 42;
     const double value2 = 43.6;
-    buffer.push_back(value1);
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
     REQUIRE(1 == buffer.populated());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE(value2 == buffer.front());
     REQUIRE(1 == buffer.populated());
 }
@@ -40,10 +41,10 @@ TEST_CASE("circular_buffer__buffer_of_size_two_with_two_added") {
     static_assert(size == buffer.size(), "");
     const double value1 = 42;
     const double value2 = 43.6;
-    buffer.push_back(value1);
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
     REQUIRE(1 == buffer.populated());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE(value1 == buffer.front());
     REQUIRE(2 == buffer.populated());
 }
@@ -55,54 +56,54 @@ TEST_CASE("circular_buffer__buffer_of_size_two_with_three_added") {
     const double value1 = 42;
     const double value2 = 46;
     const double value3 = 14;
-    buffer.push_back(value1);
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
     REQUIRE(1 == buffer.populated());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE(value1 == buffer.front());
     REQUIRE(2 == buffer.populated());
-    buffer.push_back(value3);
+    buffer.push(value3);
     REQUIRE(value2 == buffer.front());
     REQUIRE(2 == buffer.populated());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_one_pop_front") {
+TEST_CASE("circular_buffer__buffer_of_size_one_pop") {
     circular_buffer<double, 1> buffer;
     const double value1 = 42;
     const double value2 = 46;
-    REQUIRE(0 == buffer.front());
-    buffer.push_back(value1);
+    REQUIRE(buffer.empty());
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE(value2 == buffer.front());
-    buffer.pop_front();
-    REQUIRE(0 == buffer.front());
-    buffer.push_back(value1);
+    buffer.pop();
+    REQUIRE(buffer.empty());
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_two_pop_front") {
+TEST_CASE("circular_buffer__buffer_of_size_two_pop") {
     circular_buffer<double, 2> buffer;
     REQUIRE(buffer.empty());
     REQUIRE_FALSE(buffer.full());
     const double value1 = 42;
     const double value2 = 46;
-    buffer.push_back(value1);
+    buffer.push(value1);
     REQUIRE_FALSE(buffer.empty());
     REQUIRE_FALSE(buffer.full());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE_FALSE(buffer.empty());
     REQUIRE(buffer.full());
     REQUIRE(value1 == buffer.front());
-    buffer.pop_front();
+    buffer.pop();
     REQUIRE(value2 == buffer.front());
-    buffer.pop_front();
-    REQUIRE(0 == buffer.front());
+    buffer.pop();
+    REQUIRE(buffer.empty());
     REQUIRE(buffer.empty());
     REQUIRE_FALSE(buffer.full());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_three_with_push_back_overload") {
+TEST_CASE("circular_buffer__buffer_of_size_three_with_push_overload") {
     circular_buffer<double, 3> buffer;
     const double value1 = 42;
     const double value2 = 46;
@@ -112,25 +113,25 @@ TEST_CASE("circular_buffer__buffer_of_size_three_with_push_back_overload") {
     const double value6 = 9;
     const double value7 = 10;
     const double value8 = 11;
-    buffer.push_back(42);
+    buffer.push(42.);
     REQUIRE(value1 == buffer.front());
-    buffer.push_back(46);
+    buffer.push(46.);
     REQUIRE(value1 == buffer.front());
-    buffer.push_back(14);
+    buffer.push(14.);
     REQUIRE(value1 == buffer.front());
-    buffer.push_back(7);
+    buffer.push(7.);
     REQUIRE(value2 == buffer.front());
-    buffer.push_back(8);
+    buffer.push(8.);
     REQUIRE(value3 == buffer.front());
-    buffer.push_back(9);
+    buffer.push(9.);
     REQUIRE(value4 == buffer.front());
-    buffer.push_back(10);
+    buffer.push(10.);
     REQUIRE(value5 == buffer.front());
-    buffer.push_back(11);
+    buffer.push(11.);
     REQUIRE(value6 == buffer.front());
-    buffer.push_back(12);
+    buffer.push(12.);
     REQUIRE(value7 == buffer.front());
-    buffer.push_back(13);
+    buffer.push(13.);
     REQUIRE(value8 == buffer.front());
 }
 
@@ -144,20 +145,20 @@ TEST_CASE("circular_buffer__buffer_of_size_three_push_and_pop") {
     const double value6 = 9;
     const double value7 = 10;
     const double value8 = 11;
-    REQUIRE(0 == buffer.front());
-    REQUIRE_FALSE(buffer.pop_front());
-    buffer.push_back(value1);
+    REQUIRE(buffer.empty());
+    REQUIRE_FALSE(buffer.pop());
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(buffer.pop_front());
-    REQUIRE(0 == buffer.front());
-    REQUIRE_FALSE(buffer.pop_front());
-    buffer.push_back(value1);
+    REQUIRE(buffer.pop());
+    REQUIRE(buffer.empty());
+    REQUIRE_FALSE(buffer.pop());
+    buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    buffer.push_back(value2);
+    buffer.push(value2);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(buffer.pop_front());
+    REQUIRE(buffer.pop());
     REQUIRE(value2 == buffer.front());
-    buffer.push_back(value1);
-    buffer.push_back(value3);
+    buffer.push(value1);
+    buffer.push(value3);
     REQUIRE(value2 == buffer.front());
 }
