e807cfd8ba188fc18b1d98d9a4b8d9a95835dd5e
Guan Wang <ggwangguan@gmail.com>
2018-07-17 20:05:19 +1200

    rename the attributes and the methods

--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -3,71 +3,71 @@
 
 using gem::circular_buffer;
 
-TEST_CASE("circular_buffer__buffer_of_size_one_no_elements") {
-    constexpr int size = 1;
-    circular_buffer<double, size> buffer;
-    static_assert(size == buffer.size(), "");
-    REQUIRE(0 == buffer.populated());
+TEST_CASE("circular_buffer__buffer_of_capacity_one_no_elements") {
+    constexpr int capacity = 1;
+    circular_buffer<double, capacity> buffer;
+    static_assert(capacity == buffer.capacity(), "");
+    REQUIRE(0 == buffer.size());
     REQUIRE(buffer.empty());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_one_with_one_added") {
-    constexpr int size = 1;
-    circular_buffer<double, size> buffer;
-    static_assert(size == buffer.size(), "");
+TEST_CASE("circular_buffer__buffer_of_capacity_one_with_one_added") {
+    constexpr int capacity = 1;
+    circular_buffer<double, capacity> buffer;
+    static_assert(capacity == buffer.capacity(), "");
     const double value = 42;
     buffer.push(value);
     REQUIRE(value == buffer.front());
-    REQUIRE(1 == buffer.populated());
+    REQUIRE(1 == buffer.size());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_one_with_two_added") {
-    constexpr int size = 1;
-    circular_buffer<double, size> buffer;
-    static_assert(size == buffer.size(), "");
+TEST_CASE("circular_buffer__buffer_of_capacity_one_with_two_added") {
+    constexpr int capacity = 1;
+    circular_buffer<double, capacity> buffer;
+    static_assert(capacity == buffer.capacity(), "");
     const double value1 = 42;
     const double value2 = 43.6;
     buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(1 == buffer.populated());
+    REQUIRE(1 == buffer.size());
     buffer.push(value2);
     REQUIRE(value2 == buffer.front());
-    REQUIRE(1 == buffer.populated());
+    REQUIRE(1 == buffer.size());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_two_with_two_added") {
-    constexpr int size = 2;
-    circular_buffer<double, size> buffer;
-    static_assert(size == buffer.size(), "");
+TEST_CASE("circular_buffer__buffer_of_capacity_two_with_two_added") {
+    constexpr int capacity = 2;
+    circular_buffer<double, capacity> buffer;
+    static_assert(capacity == buffer.capacity(), "");
     const double value1 = 42;
     const double value2 = 43.6;
     buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(1 == buffer.populated());
+    REQUIRE(1 == buffer.size());
     buffer.push(value2);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(2 == buffer.populated());
+    REQUIRE(2 == buffer.size());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_two_with_three_added") {
-    constexpr int size = 2;
-    circular_buffer<double, size> buffer;
-    static_assert(size == buffer.size(), "");
+TEST_CASE("circular_buffer__buffer_of_capacity_two_with_three_added") {
+    constexpr int capacity = 2;
+    circular_buffer<double, capacity> buffer;
+    static_assert(capacity == buffer.capacity(), "");
     const double value1 = 42;
     const double value2 = 46;
     const double value3 = 14;
     buffer.push(value1);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(1 == buffer.populated());
+    REQUIRE(1 == buffer.size());
     buffer.push(value2);
     REQUIRE(value1 == buffer.front());
-    REQUIRE(2 == buffer.populated());
+    REQUIRE(2 == buffer.size());
     buffer.push(value3);
     REQUIRE(value2 == buffer.front());
-    REQUIRE(2 == buffer.populated());
+    REQUIRE(2 == buffer.size());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_one_pop") {
+TEST_CASE("circular_buffer__buffer_of_capacity_one_pop") {
     circular_buffer<double, 1> buffer;
     const double value1 = 42;
     const double value2 = 46;
@@ -82,7 +82,7 @@ TEST_CASE("circular_buffer__buffer_of_size_one_pop") {
     REQUIRE(value1 == buffer.front());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_two_pop") {
+TEST_CASE("circular_buffer__buffer_of_capacity_two_pop") {
     circular_buffer<double, 2> buffer;
     REQUIRE(buffer.empty());
     REQUIRE_FALSE(buffer.full());
@@ -103,7 +103,7 @@ TEST_CASE("circular_buffer__buffer_of_size_two_pop") {
     REQUIRE_FALSE(buffer.full());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_three_with_push_overload") {
+TEST_CASE("circular_buffer__buffer_of_capacity_three_with_push_overload") {
     circular_buffer<double, 3> buffer;
     const double value1 = 42;
     const double value2 = 46;
@@ -135,7 +135,7 @@ TEST_CASE("circular_buffer__buffer_of_size_three_with_push_overload") {
     REQUIRE(value8 == buffer.front());
 }
 
-TEST_CASE("circular_buffer__buffer_of_size_three_push_and_pop") {
+TEST_CASE("circular_buffer__buffer_of_capacity_three_push_and_pop") {
     circular_buffer<double, 3> buffer;
     const double value1 = 42;
     const double value2 = 46;
@@ -159,4 +159,44 @@ TEST_CASE("circular_buffer__buffer_of_size_three_push_and_pop") {
     buffer.push(value1);
     buffer.push(value3);
     REQUIRE(value2 == buffer.front());
+}
+
+TEST_CASE("circular_buffer__buffer_of_capacity_three_push_and_pop_guanpath") {
+    circular_buffer<double, 3> buffer;
+    const double value1 = 1;
+    const double value2 = 2;
+    const double value3 = 3;
+    const double value4 = 4;
+    const double value5 = 5;
+    const double value6 = 6;
+    const double value7 = 7;
+    const double value8 = 8;
+    const double value9 = 9;
+    const double value10 = 10;
+    const double value11 = 11;
+    const double value12 = 12;
+
+    REQUIRE(buffer.empty());
+    buffer.push(value1);
+    buffer.push(value2);
+    buffer.push(value3);
+    buffer.push(value4);
+    buffer.pop();
+    REQUIRE(value3 == buffer.front());
+    buffer.push(value5);
+    REQUIRE(value3 == buffer.front());
+    buffer.pop();
+    REQUIRE(value4 == buffer.front());
+    buffer.pop();
+    REQUIRE(value5 == buffer.front());
+    buffer.pop();
+    buffer.push(value8);
+    REQUIRE(value8 == buffer.front());
+    buffer.push(value9);
+    buffer.push(value10);
+    REQUIRE(value8 == buffer.front());
+    buffer.pop();
+    buffer.pop();
+
+    REQUIRE(value10 == buffer.front());
 }
